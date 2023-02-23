9f8a22b8f86ebc126556051ec56f218937e3a4b4
Kim Banga <bangakim227@gmail.com>
2023-02-23 16:34:29 +0000

    initial commit

---
+++ src/main/java/com/kimknife/springbootecs/SpringBootEcsApplication.java
@@ -0,0 +1,13 @@
+package com.kimknife.springbootecs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kimknife/springbootecs/SpringBootEcsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kimknife.springbootecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
